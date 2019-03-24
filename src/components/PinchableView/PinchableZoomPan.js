198c84122a07214a40c0e09ebc9453e86678d99e
Bonchouuur <n.chauris@gmail.com>
2019-03-24 21:30:44 +0100

    chore(Observable) : Decrease version

--- src/components/PinchableView/PinchableZoomPan.js
+++ src/components/PinchableView/PinchableZoomPan.js
@@ -2,8 +2,8 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { Observable } from 'rxjs/Observable';
-import { fromEvent } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
+// import { fromEvent } from 'rxjs';
 import throttle from 'lodash.throttle';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/do';
@@ -101,12 +101,18 @@ class PinchableZoomPan extends Component {
 
   handlePinch() {
     const domNode = this.root;
-    const touchStart = fromEvent(
+    const touchStart = Observable.fromEvent(
       domNode,
       isTouch() ? 'touchstart' : 'mousedown',
     );
-    const touchMove = fromEvent(window, isTouch() ? 'touchmove' : 'mousemove');
-    const touchEnd = fromEvent(window, isTouch() ? 'touchend' : 'mouseup');
+    const touchMove = Observable.fromEvent(
+      window,
+      isTouch() ? 'touchmove' : 'mousemove',
+    );
+    const touchEnd = Observable.fromEvent(
+      window,
+      isTouch() ? 'touchend' : 'mouseup',
+    );
 
     function translatePos(point, size) {
       return {
