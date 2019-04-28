1b8bf19087e4948a2d2e643f035d826b637fc09a
Bonchouuur <n.chauris@gmail.com>
2019-04-28 23:21:22 +0200

    fix(Style) : Apply style to main component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
 import BoardScreen from './components/DrawingBoard/BoardScreen';
 import BoardProvider from './components/DrawingBoard/BoardProvider';
 
+const GlobalStyle = createGlobalStyle`
+.top-bordered {
+  border-top: 1px solid #dbdbdb;
+}
+
+.left-bordered {
+  border-left: 1px solid #dbdbdb;
+}
+
+.right-bordered {
+  border-right: 1px solid #dbdbdb;
+}
+
+.bottom-bordered {
+  border-bottom: 1px solid #dbdbdb;
+}
+
+.Popover {
+  z-index: 99999;
+}
+
+.Popover-body {
+  background-color: white;
+  border: 1px solid #dbdbdb;
+  border-radius: 5px;
+}
+
+.Popover-tipShape {
+  fill: #dbdbdb;
+}
+`;
+
 const DrawMeAUnicorn = ({ onSave }) => (
   <BoardProvider>
+    <GlobalStyle />
     <BoardScreen onSave={onSave} />
   </BoardProvider>
 );
